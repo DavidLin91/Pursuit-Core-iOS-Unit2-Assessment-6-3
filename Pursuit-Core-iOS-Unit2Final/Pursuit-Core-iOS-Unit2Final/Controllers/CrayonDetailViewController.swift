73479b2c833f82d69b98f3b2e0372eb50f434063
David <lin.david985@gmail.com>
2019-11-22 17:44:21 -0500

    made constraints. fixed bugs

--- Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/CrayonDetailViewController.swift
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/CrayonDetailViewController.swift
@@ -26,21 +26,29 @@ class CrayonDetailViewController: UIViewController {
         redColorChangeSlider.value = Float(color.red/255)
         greenColorChangeSlider.value = Float(color.green/255)
         blueColorChangeSlider.value = Float(color.blue/255)
+        alphaStepper.value = Double(Float(1.0))
     }
     
-    @IBAction func redSlider(_ sender: UISlider) {
-        var currentFloat = color.red
-        redColorChangeSlider.value = sender.value
+    @IBAction func colorChangeSliders(_ sender: AnyObject) {
+        switch sender.tag {
+        case 0:
+            redColorChangeSlider.value = sender.value
+        case 1:
+            greenColorChangeSlider.value = sender.value
+        case 2:
+            blueColorChangeSlider.value = sender.value
+        case 3:
+            alphaStepper.value = sender.value
+        default:
+            break
+        }
+        let newRedNumber = redColorChangeSlider.value
+        let newGreenNumber = greenColorChangeSlider.value
+        let newBlueNumber = blueColorChangeSlider.value
+        let newAlphaNum = alphaStepper.value
+        self.view.backgroundColor = UIColor(red: CGFloat(newRedNumber), green: CGFloat(newGreenNumber), blue: CGFloat(newBlueNumber), alpha: CGFloat(newAlphaNum))
     }
-    
-    @IBAction func greenSlider(_ sender: UISlider) {
-    }
-    
-    @IBAction func blueSlider(_ sender: UISlider) {
-    }
-    
-    @IBAction func alphaStepper(_ sender: UIStepper) {
-    }
+
     
     @IBAction func colorResetButton(_ sender: UIButton) {
     self.view.backgroundColor = UIColor(red: CGFloat(color.red)/255, green: CGFloat(color.green)/255, blue: CGFloat(color.blue)/255, alpha: CGFloat(1.0))
@@ -48,6 +56,4 @@ class CrayonDetailViewController: UIViewController {
     greenColorChangeSlider.value = Float(color.green/255)
     blueColorChangeSlider.value = Float(color.blue/255)
     }
-    
-    
 }
